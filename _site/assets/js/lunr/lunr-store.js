48d589aafd2b9feac90be6cd9373dae337e5a91c
jie <penguinpig0120@gmail.com>
2023-06-05 08:52:01 +0800

    更新內容，分主要3個標籤

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,31 @@
+var store = [{
+        "title": "車輛基本資訊",
+        "excerpt":"車輛基本資訊   油水      機油交換量            不換濾心: 2.5L       交換濾心: 2.7L       引擎全拆: 3.2L           原廠規格      前齒盤:??T   後齒盤   鍊條尺寸:520，112目   原廠扭力參考建議值      前輪軸心：54N.m   後輪軸心：88N.m   鏈條調節器：27N.m   空濾外蓋：1.1N.m   機油濾芯：26N.m   洩油螺絲：30N.m   卡鉗螺絲：30N.m   輪軸固定螺絲：22N.m  ","categories": [],
+        "tags": [],
+        "url": "/docs/CKTGs/bikeComSet/",
+        "teaser": null
+      },{
+        "title": "友善商家",
+        "excerpt":"北部      台北北投-霜暮重機02-28958063   台北信義-辰昶車業02-27328138   台北大同-普隆貢工房02-25576319   新北蘆洲-車神重機0975023458   中部      台中北區-焦點車業04-22085070   台中西區-泛雅車業0979215200   台中北屯-luxurious0980959507   台中南屯-簡單車業04-22939399   南投市。-紳暘車業0963576477   南部      台南新營-偉傑車業06-6323816   台南新市-軒瑋重車06-5896784   台南安南-武吉重車06-3552088   台南安南-伍陸車業06-3560858   台南安南-武揚車業0963331157   台南永康-豪工房0955139404   台南永康-瑞奇重機06-2052725   台南仁德-捷勁車業0925261658   台南安平-華信機車06-2997567   台南安平-新展車業06-2995206   高雄鳳山-阿毅輪胎屋0915103621   高雄三民-丹尼車業0986392571   高雄三民-熊本二輪07-3231259   高雄本田-謝東育技師   高雄大樹-騎士殿0920863465  ","categories": [],
+        "tags": [],
+        "url": "/docs/CKTGs/friendlyStore/",
+        "teaser": null
+      },{
+        "title": "原廠後避震預載調整",
+        "excerpt":"原廠後避震預載調整                    ","categories": [],
+        "tags": [],
+        "url": "/docs/CKTGs/readShockAdjust/",
+        "teaser": null
+      },{
+        "title": "第一篇文章",
+        "excerpt":"        測試            Lubada豪爽哦      ","categories": [],
+        "tags": [],
+        "url": "/FirstPost/",
+        "teaser": null
+      },{
+        "title": "設定個人BLOG(GithubPage + jekyll)",
+        "excerpt":"官方簡介   Jekyll一種由Ruby寫出來的生成靜態網站的套件，支援markdown格式的文件。   安裝步驟      安裝Ruby            https://www.ruby-lang.org/zh_tw/downloads/           檢測環境Ruby是否裝成功            cmd ruby -v             ","categories": [],
+        "tags": [],
+        "url": "/docs/studys/quickStart/",
+        "teaser": null
+      }]

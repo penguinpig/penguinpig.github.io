d4f3cd6b70ac976542804dd0219130283263a229
Penguinpig <penguinpig0120@gmail.com>
2024-09-11 15:07:18 +0800

    修改指令

--- AutoDeploy.groovy
+++ AutoDeploy.groovy
@@ -5,20 +5,24 @@ pipeline{
         }
     }
     stages {
+        stage('Setting config'){
+            sh 'git config --global user.email "penguinpig0120@gmail.com"'
+            sh 'git config --global user.name  "Jenkins"'
+        }
         stage('Checkout') {
             steps {
                 git(url: 'git@github.com:penguinpig/penguinpig.github.io.git', branch: 'dev')
             }
         }
-        stage('Build') {
+        stage('Build & Deploy') {
             steps {
                 sh 'sh AutoDeploy.sh'
             }
         }
-        stage('Delivery') {
-            steps {
-                sh 'echo \'Publish artifact over SSH.\''
-            }
-        }
+        // stage('Delivery') {
+        //     steps {
+        //         sh 'echo \'Publish artifact over SSH.\''
+        //     }
+        // }
     }
 }
